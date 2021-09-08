08504c6354a9771bbb46e203cba88bbc35776672
zhh <exiaolv@qq.com>
2021-09-08 09:57:34 +0800

    添加快手mini

--- pf_mini/src/index.ts
+++ pf_mini/src/index.ts
@@ -2,6 +2,7 @@
 import * as tt_mini from "./tt_mini";
 import * as qq_mini from "./qq_mini";
 import * as wx_mini from "./wx_mini";
+import * as ks_mini from "./ks_mini";
 import * as qq_pay from "./qq_pay";
 import * as wx_pay from "./wx_pay";
 import * as pf_mini_util from "./_util";
@@ -10,6 +11,7 @@ export {
     wx_mini,
     qq_mini,
     tt_mini,
+    ks_mini,
     qq_pay,
     wx_pay,
 }
