ff60faec18018d395ba50b135dfb949fd7a8962e
lhkzh <exiaolv@qq.com>
2021-11-15 20:05:20 +0800

    up

--- fibjs_alicloud/src/index.ts
+++ fibjs_alicloud/src/index.ts
@@ -3,12 +3,13 @@ import * as ssl from "ssl";
 ssl.loadRootCerts();
 import {RPCClient} from "./RPCClient";
 import {ROAClient} from "./ROAClient";
-import {OSSObject} from "./OSSClient";
+import {OSSBucket, OSSObject} from "./OSSClient";
 import {STS} from "./STS";
 export{
     RPCClient,
     ROAClient,
     OSSObject,
+    OSSBucket,
     STS
 }
 
