39143775f9b69161cd9cb04c84f4e18fda54a239
zhh <exiaolv@qq.com>
2021-11-22 11:27:10 +0800

    parse

--- pf_xml/src/index.ts
+++ pf_xml/src/index.ts
@@ -112,3 +112,47 @@ function sub_rows_to_obj(arr_sub_objs){
     }
     return arr_sub_objs;
 }
+
+/**
+ * 匹配 xml 标签对
+ * 微信的数据目前仅简单标签对形式
+ */
+const RE_FIELD = /<(\w+)>([\s\S]*?)<\/\1>/g;
+/**
+ * 解析 xml 数据
+ * @param xml 数据
+ */
+export function xmlToObjSimple(xml:string, toArrayFields:string[]=[]) {
+    const items = []; // 存储字段匹配结果
+    if (!RE_FIELD.test(xml)) {
+        return xml.replace(/<!\[CDATA\[(.*)\]\]>/, '$1');
+    }
+    xml.replace(RE_FIELD, (m, field, value) => {
+        items.push({ field, value: xmlToObjSimple(value, toArrayFields) });
+        return m;
+    });
+    const keys = items.map(({ field }) => field);
+    const isArray = keys.length > 1 && [...new Set(keys)].length === 1;
+    const target = {};
+    if (isArray) {
+        const arr = [];
+        target[items[0].field] = arr;
+        items.forEach((it) => arr.push(it.value));
+    }
+    else {
+        items.forEach((it) => {
+            if(target.hasOwnProperty(it.field) || toArrayFields.includes(it.field)){
+                if(Array.isArray(target[it.field])){
+                    target[it.field].push(it.value);
+                } else if(target.hasOwnProperty(it.field)){
+                    target[it.field]=[target[it.field], it.value];
+                } else {
+                    target[it.field]=[it.value];
+                }
+            } else {
+                target[it.field] = it.value;
+            }
+        });
+    }
+    return target;
+}
