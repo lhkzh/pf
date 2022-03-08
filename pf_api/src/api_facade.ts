742f1598563d1f8d508fe4c5956e396e14987286
qthis <exiaolv@qq.com>
2022-03-08 13:50:24 +0800

    update

--- pf_api/src/api_facade.ts
+++ pf_api/src/api_facade.ts
@@ -415,7 +415,7 @@ export function REPEATER(apiRule: ApiMethod & { toUrl: string | string[], fixPat
                 ctx.req.address = toPath;
                 ctx.res.statusCode = 0;
                 try {
-                    ctx.req.response.removeHeader("Server");
+                    ctx.req.response.clear();
                     repeater.invoke(ctx.req);
                 } finally {
                     lastFn.apply(this, [toPath, ctx.res.statusCode == 200]);
